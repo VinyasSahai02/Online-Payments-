1a6fdb3b28e6d07f38a3cb2ae9618b22caf2f1b1
vinyas <vinyassahai03@gmail.com>
2024-11-14 12:36:04 +0530

    created login form

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import {useNavigate} from 'react-router-dom'
+import { Col, Form, Row } from "antd";
 
 function Login() {
+  const navigate=useNavigate()
+  const onFinish = (values) => {};
+
   return (
-    <div>Login</div>
-  )
+    <div className="bg-primary flex items-center justify-center h-screen">
+      <div className="card w-400 p-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl">PAYPILOT - LOGIN</h1>
+        </div>
+        <hr />
+        <Form layout="vertical" onFinish={onFinish}>
+          <Row gutter={16}>
+            <Col span={24}>
+              <Form.Item label="Email" name="email">
+                <input type="text" />
+              </Form.Item>
+            </Col>
+
+            <Col span={24}>
+              <Form.Item label="Password" name="password">
+                <input type="password" />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <button className="primary-contained-btn w-100" type="submit">
+            Login
+          </button>
+          <h1 className="text-sm underline mt-2" onClick={()=>{navigate("/register")}}>Not a member , Click Here To Register</h1>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
